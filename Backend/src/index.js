0836d055ce48efcc2dd7c1ffc3af864665fe9409
jocada123 <69184452+jocada123@users.noreply.github.com>
2023-07-01 09:52:49 -0500

    cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,10 +3,11 @@ import usersRoutes from "./routes/users.routes.js"
 import rolesRoutes from "./routes/roles.routes.js"
 import DocumentTypeRoutes from "./routes/document_type.routes.js"
 import loginRoutes from "./routes/login.routes.js"
+import cors from 'cors';
  
 const app = express()
 app.use(express.json())
-
+app.use(cors());
 app.use("/api/",usersRoutes);
 app.use("/api/",rolesRoutes);
 app.use("/api/",DocumentTypeRoutes);
